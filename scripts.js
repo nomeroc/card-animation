03e0ba91b285f8dd444a29636c869fe0704ab2fb
nomeroc <similardmykhalets@gmail.com>
2024-05-22 14:57:42 +0300

    fix

--- scripts.js
+++ scripts.js
@@ -11,19 +11,59 @@ const buttonsBlock = document.querySelector('.buttons');
 const imageDetails = {
     'Мировой рынок': {
         src: '/assets/pic.png',
-        transform: 'translate(0, -10%) scale(0.75) rotate(0deg)',
+        styles: {
+            bottom: '-85%',
+            transform: 'translateX(0) rotate(0) scale(0.7)',
+        },
+        hoverStyles: {
+            transform: 'translateX(0) rotate(0) scale(1.1)',
+        },
+        endStyles: {
+            bottom: '-35%',
+            transform: 'translateX(0) rotate(0) scale(1)',
+        },
     },
-    Економика: {
+    'Економика': {
         src: '/assets/pic2.png',
-        transform: 'translate(0%, -90%) scale(0.8) rotate(-10deg)',
+        styles: {
+            bottom: '-25%',
+            transform: 'translate(0) scale(0.7)',
+        },
+        hoverStyles: {
+            transform: 'translate(0) scale(1)',
+        },
+        endStyles: {
+            bottom: '-2.5%',
+            transform: 'translate(0) scale(0.9)',
+        },
     },
-    Технологии: {
-        src: '/assets/pic3.png',
-        transform: 'translate(10%, -30%) scale(0.9) rotate(-20deg)',
-    },
-    Членство: {
+    'Технологии': {
         src: '/assets/pic4.png',
-        transform: 'translate(35%, -20%) scale(0.9) rotate(-15deg)',
+        styles: {
+            bottom: '-45%',
+            transform: 'translateX(30%) rotate(-15deg) scale(0.6)',
+        },
+        hoverStyles: {
+            transform: 'translateX(30%) rotate(-15deg) scale(1.1)',
+        },
+        endStyles: {
+            bottom: '-25%',
+            transform: 'translateX(30%) rotate(-15deg) scale(1)',
+        },
+    },
+    'Членство': {
+        src: '/assets/pic3.png',
+        styles: {
+            bottom: '-35%',
+            transform: 'translateX(25%) rotate(-20.3deg) scale(0.45)',
+        },
+        hoverStyles: {
+            transform: 'translateX(15%) rotate(-20.3deg) scale(1)',
+        },
+        endStyles: {
+            bottom: '-12%',
+            transform: 'translateX(15%) rotate(-20.3deg) scale(0.9)',
+        },
     },
 };
 
@@ -39,7 +79,8 @@ buttons.forEach((button) => {
         // Fade out the current content
         titleElement.classList.add('fade-out');
         textElement.classList.add('fade-out');
-        imageElement.classList.add('fade-out');
+        // imageElement.classList.add('fade-out');
+        
 
         // Wait for the fade-out transition to complete
         setTimeout(() => {
@@ -47,21 +88,59 @@ buttons.forEach((button) => {
             titleElement.textContent = button.getAttribute('data-title');
             textElement.textContent = button.getAttribute('data-text');
 
-            // Update the image source and transform
+            // Update the image source and styles
             const imageDetail = imageDetails[button.getAttribute('data-title')];
+            imageElement.style.transition = 'none';
             imageElement.src = imageDetail.src;
-            imageElement.style.transform = imageDetail.transform;
+            Object.assign(imageElement.style, imageDetail.styles);
+
+            imageElement.classList.remove('appear-animation');
+            imageElement.classList.remove('image-hover-animation');
+
+            void imageElement.offsetWidth;
+            imageElement.style.transition = 'bottom 1s, transform 0.5s';
+
+            // Add appear animation if defined
+            if (imageDetail.animation) {
+                imageElement.classList.add(imageDetail.animation);
+            }
+
+            imageElement.style.setProperty('--initial-transform', imageDetail.endStyles.transform);
+            imageElement.style.setProperty('--hover-transform', imageDetail.hoverStyles.transform);
+
+            setTimeout(() => {
+                Object.assign(imageElement.style, imageDetail.endStyles);
+            }, 500);
 
             // Fade in the new content
             titleElement.classList.remove('fade-out');
             textElement.classList.remove('fade-out');
-            imageElement.classList.remove('fade-out');
-            imageElement.classList.add('fade-in');
+            // imageElement.classList.remove('fade-out');
 
-            // Remove fade-in class after animation
-            setTimeout(() => {
-                imageElement.classList.remove('fade-in');
-            }, 1000); // Match this duration to CSS transition duration
+            const handleMouseOver = () => {
+                imageElement.classList.add('image-hover-animation');
+            };
+            const handleMouseOut = () => {
+            imageElement.classList.remove('image-hover-animation');
+            Object.assign(imageElement.style, imageDetail.endStyles);
+        };
+
+            // Remove any previous hover event listeners
+            imageElement.removeEventListener('mouseover', handleMouseOver);
+            imageElement.removeEventListener('mouseout', handleMouseOut);
+
+            // Add new hover event listeners
+            imageElement.addEventListener('mouseover', handleMouseOver);
+            imageElement.addEventListener('mouseout', handleMouseOut);
+        
         }, 500); // Duration should match the CSS transition duration
     });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the first button and simulate a click to initialize
+    const firstButton = buttons[0];
+    if (firstButton) {
+        firstButton.click();
+    }
 });
